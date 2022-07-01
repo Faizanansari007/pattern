28183f8edf2cbd1a50566cca84ba9f9ec2109110
FAIZAN <FAIZAN@FAIZAN-PC>
2022-07-01 08:35:32 +0530

    push the pattern program

---
+++ pattern/src/pattern/Patter4.java
@@ -0,0 +1,19 @@
+package pattern;
+
+public class Patter4 {
+
+	public static void main(String[] args) {
+	int n=5;
+	for(int i=0;i<=n;i++) {
+		for(int j=i;j<=n-i;j++) {
+			System.out.print(" ");
+		}
+		for(int k=0;k<=i;k++) {
+			System.out.print(" *");
+		}
+		System.out.println("");
+	}
+
+	}
+
+}
---
+++ pattern/src/pattern/Pattern3.java
@@ -0,0 +1,19 @@
+package pattern;
+
+public class Pattern3 {
+
+	public static void main(String[] args) {
+		
+			int number=1;
+			for(int i=0;i<=4;i++) {
+				
+				for(int j=1;j<i+1;j++) {
+					System.out.print(number++ +" ");
+					
+				}
+				System.out.println();
+			
+			}
+			 
+
+}}
